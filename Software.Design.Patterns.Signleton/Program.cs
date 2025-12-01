c641714c9b79a9e22a5668ef3ffa476f435d641d
Ye7ia01 <whoami.iamcipher@gmail.com>
2025-12-01 19:52:58 +0200

    initial commit

---
+++ Software.Design.Patterns.Signleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using Software.Design.Patterns.Signleton;
+
+var loadBalancer1 = SingletonLoadBalancer.GetInstance();
+var loadBalancer2 = SingletonLoadBalancer.GetInstance();
+var loadBalancer3 = SingletonLoadBalancer.GetInstance();
+
+Console.WriteLine(loadBalancer1 == loadBalancer2);
+Console.WriteLine(loadBalancer2 == loadBalancer3);
+Console.WriteLine(loadBalancer1 == loadBalancer3);
+
+if (loadBalancer1 == loadBalancer2 && loadBalancer1 == loadBalancer3 && loadBalancer2 == loadBalancer3)
+{
+    Console.WriteLine("The same instance of the load balancer was returned");
+}
+else
+{
+    Console.WriteLine("Different instances of the load balancer were returned");
+}
+Console.WriteLine();
+
+Console.WriteLine("Hello, World!");
---
+++ Software.Design.Patterns.Signleton/SingletonLoadBalancer.cs
@@ -0,0 +1,38 @@
+﻿namespace Software.Design.Patterns.Signleton;
+
+public class SingletonLoadBalancer
+{
+    private static SingletonLoadBalancer _instance;
+    // Static key object to be referenced by all threads
+    private static readonly object _padlock = new object();
+    private List<string> _servers = new List<string>();
+    
+    // Private / Protected constructor to prevent instantiation
+    private SingletonLoadBalancer()
+    {
+        _servers.Add("Server 1");
+        _servers.Add("Server 2");
+        _servers.Add("Server 3");
+    }
+
+    public static SingletonLoadBalancer GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_padlock)
+            {
+                // Critical Section for Multithreading
+                
+                // The second If statement (repeated) is necessary
+                // since if a thread A is inside the critical section and thread B waiting for lock (after first if)
+                // the second If statement will prevent thread B from creating another instance
+                if (_instance == null)
+                {
+                    _instance = new SingletonLoadBalancer();
+                }
+
+            }
+        }
+        return _instance;
+    }
+}
